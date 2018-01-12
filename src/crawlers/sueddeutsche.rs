e3d0c7cbba31c98059572fbc29ac8b62448fcc06
Florian Schnell <florian.schnell@netlight.com>
2018-01-12 07:13:55 +0100

    improve performace of sueddeutsche crawler.

--- src/crawlers/sueddeutsche.rs
+++ src/crawlers/sueddeutsche.rs
@@ -5,6 +5,7 @@ extern crate std;
 use crawlers::{Crawler, Error};
 use kuchiki::{ElementData, NodeDataRef};
 use models::{Cities, FlatData};
+use std::ops::Deref;
 
 pub struct Sueddeutsche {
   pub host: String,
@@ -44,47 +45,40 @@ impl Crawler for Sueddeutsche {
   }
 
   fn transform_result<'a>(&self, result: NodeDataRef<ElementData>) -> Result<FlatData, Error> {
-    let rent = Self::get_text(&result, ".hitPrice")?.replace("&nbsp;", " ");
-    let squaremeters = Self::get_text(&result, ".hitRoomsDiv")?
-      .split(", ")
-      .map(|s| s.to_owned())
-      .collect::<Vec<_>>()
-      .get(1)
-      .map(|s| s.to_owned());
-    let rooms = Self::get_text(&result, ".hitRoomsDiv")?
-      .split(", ")
-      .map(|s| s.to_owned())
-      .collect::<Vec<_>>()
-      .get(1)
-      .map(|s| s.to_owned());
+    let hit_rooms_div_text = Self::get_text(&result, ".hitRoomsDiv")?;
+    let hit_rooms_div_elements: Vec<&str> = hit_rooms_div_text.split(", ").collect();
+    let squaremeters_opt: Option<&&str> = hit_rooms_div_elements.get(0);
+    let rooms_opt: Option<&&str> = hit_rooms_div_elements.get(1);
+
+    let hit_regions_text = Self::get_text(&result, ".hitRegionTxt")?.replace("\t", "");
+    let hit_regions_elements: Vec<&str> = hit_regions_text.split("\n").collect();
+    let address_opt = hit_regions_elements.get(2);
+
     let title = Self::get_text(&result, ".hitHeadline")?
       .replace("\t", "")
       .replace("\n", "");
-    let address = Self::get_text(&result, ".hitRegionTxt")?
-      .replace("\t", "")
-      .split("\n")
-      .map(|s| s.to_owned())
-      .collect::<Vec<_>>()
-      .get(2)
-      .map(|s| s.to_owned());
+
+    let rent = Self::get_text(&result, ".hitPrice")?.replace("&nbsp;", " ");
+
     let externalid = Self::get_attr(&result, "id")?.replace("idHitRowList", "");
 
-    let err_msg = format!(
-      "Information is incomplete: {:?}, {:?}, {:?}!",
-      squaremeters,
-      rooms,
-      address
-    );
-    match (squaremeters, rooms, address) {
-      (Some(sqm), Some(rms), Some(addr)) => Ok(FlatData {
+    match (&squaremeters_opt, &rooms_opt, &address_opt) {
+      (&Some(squaremeters), &Some(rooms), &Some(address)) => Ok(FlatData {
         rent: Self::parse_number(rent)?,
-        squaremeters: Self::parse_number(sqm)?,
-        address: addr.to_owned(),
+        squaremeters: Self::parse_number(squaremeters.deref().to_owned())?,
+        address: address.deref().to_owned(),
         title,
-        rooms: Self::parse_number(rms.to_owned())?,
+        rooms: Self::parse_number(rooms.deref().to_owned())?,
         externalid,
       }),
-      _ => Err(Error { message: err_msg }),
+      _ => Err(Error {
+        message: format!(
+          "Information is incomplete: {:?}, {:?}, {:?}!",
+          squaremeters_opt,
+          rooms_opt,
+          address_opt
+        ),
+      }),
     }
   }
 }
